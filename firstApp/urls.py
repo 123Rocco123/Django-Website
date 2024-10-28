344a8988097011ebdc862f6abfe591c39baa42d6
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2024-10-28 02:31:29 +0000

    Create urls.py

---
+++ firstApp/urls.py
@@ -0,0 +1,16 @@
+# Used to import the paths function from Django
+from django.urls import path, include
+# Used to import the views file from the same directory
+from . import views
+
+urlpatterns = [
+    # An empty path, "", signifies that we want it to me the homepage
+        # The view.index is what handles the logic
+        # The "name = ..." parameter is used to set the name of the page
+    path("", views.index, name = "index"),
+    # Contains the view link to the pricing page
+    path("pricing/", views.pricing, name = "pricing"),
+
+    # Different path means that its a subdirectory
+    path("idea", views.projectIdea, name = "projectIdea"),
+]
