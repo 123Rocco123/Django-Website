5919a0cf1a856a96ee528b07fa6a487192ee90ad
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2024-11-09 14:40:02 +0000

    Create aboutme.js

---
+++ firstApp/static/firstApp/aboutme.js
@@ -0,0 +1,14 @@
+document.getElementById("imageButton").onclick = function() {
+    var dropdown = document.getElementById("imageDropdown");
+    // Set the width of the dropdown to match the image width
+    dropdown.style.width = this.offsetWidth + 'px'; // Match the image width
+    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+};
+
+document.getElementById("imageButton").onclick = function() {
+    var dropdown = document.getElementById("imageDropdown");
+    // Set the width of the dropdown to match the image width
+    dropdown.style.width = this.offsetWidth + 'px'; // Match the image width
+    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+};
+
