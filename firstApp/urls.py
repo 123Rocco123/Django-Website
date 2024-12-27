b6eefc77d29eda238219c38e410163ad34dd1fe0
Rocco Petruccio <petruccio.rocco@gmail.com>
2024-12-27 14:11:43 +0100

    Update urls.py

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('get-stock-articles/', views.getStockArticles, name='get_stock_articles'),
     path('get-stock-predictions/', views.returnPredictionModels, name='get_stock_models'),
     path('get-model-prediction/', views.get_model_prediction, name='get-model-prediction'),
+    path('get-stock-values/', views.getStockValues, name='get-stock-values'),
     
     # Different path means that its a subdirectory
     path("idea", views.projectIdea, name = "projectIdea"),
